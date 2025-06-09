c86b9be5cd171e1f2fcf6b9e43f789e373332a36
JBezzolo <121461171+juanbezzolo@users.noreply.github.com>
2025-06-08 23:07:45 -0500

    Fix login redirect

--- cultivo_project/settings.py
+++ cultivo_project/settings.py
@@ -63,3 +63,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect users to the homepage after logging in
+LOGIN_REDIRECT_URL = '/'
